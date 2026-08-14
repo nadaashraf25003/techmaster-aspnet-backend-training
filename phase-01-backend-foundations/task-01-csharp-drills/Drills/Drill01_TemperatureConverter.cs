59aab1de1508d598311dc9abea05bcb70c154256
nadaashraf25003 <nadanadaashraf25@gmail.com>
2026-08-14 23:40:37 +0300

    task-01-solution & gitignore-file

---
+++ phase-01-backend-foundations/task-01-csharp-drills/Drills/Drill01_TemperatureConverter.cs
@@ -0,0 +1,4 @@
+public class Drill01_TemperatureConverter
+{
+    
+}
---
+++ phase-01-backend-foundations/task-01-csharp-drills/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("=== C# Drills ===");
+
+Console.WriteLine("1. Temperature Converter");
+
+Console.Write("Choose a drill: ");
+
+string? choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        // Run Drill 01
+        Console.WriteLine("1. Temperature Converter");
+        break;
+
+    case "2":
+        // Run Drill 02
+        break;
+
+    case "3":
+        // Run Drill 03
+        break;
+
+    default:
+        Console.WriteLine("Invalid choice.");
+        break;
+}
